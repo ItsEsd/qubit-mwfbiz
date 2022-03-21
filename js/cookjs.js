49444c0faf11f1d8d1335e754c6ecff9ab7dc4e8
ItsEsd <81977626+ItsEsd@users.noreply.github.com>
2022-03-21 08:42:09 +0530

    Update cookjs.js

--- js/cookjs.js
+++ js/cookjs.js
@@ -93,11 +93,52 @@ var k = JSON.parse(JSON.stringify(res[0].QBstored));
 if(k!=""){
  for(var i = 0; i<=len -1;i++){
     document.getElementById("storedbitspre").style.display = "block";
-    document.getElementById("storedbitspre").innerHTML +="<button type='button' class='stqubitshow' onclick='showstqubite(this)'><div class='prestqbit'><h1>"+(i+2)/2+"</h1><hr><p>QuBit ID: "+ JSON.parse(k2[i])+"<br>Key: "+JSON.parse(k2[i+1])+
-    "</p></button>"+
-    "<input class='qbidst' value='"+k2[i]+"'><input class='keyidst' value='"+k2[i+1]+"'></div>";
+    document.getElementById("storedbitspre").innerHTML +="<button type='button' class='stqubitshow' >"+
+    "<div class='prestqbit'><div class='sharebiton' title='Copy Link' onclick='crcpbitlink(this)'><svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' class='bi bi-link-45deg' viewBox='0 0 16 16'> <path d='M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z'/> <path d='M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z'/></svg></div><h1>"+(i+2)/2+"</h1><hr><p title='Show' class='showqubitok' onclick='showstqubitok(this)'>QuBit ID: "+ JSON.parse(k2[i])+"<br>Key: "+JSON.parse(k2[i+1])+
+    "</p></div></button>"+
+    "<input class='qbidst' value='"+k2[i]+"'><input class='keyidst' value='"+k2[i+1]+"'>";
     i=i+1;
  }
 }
 }
+}
+
+function showstqubitok(label){
+  var list=document.getElementsByClassName("showqubitok");
+  list = [].slice.call(list); 
+  var posofinput = list.indexOf(label);
+  var x = document.getElementsByClassName('qbidst');
+  var y = document.getElementsByClassName('keyidst');
+       var qbid = x[posofinput].value;
+       var kqid = y[posofinput].value;
+     //  document.getElementById('storedbits').style.pointerEvents = "none";
+       document.getElementById('stqbid').value= qbid;
+       document.getElementById('stkeyid').value= kqid;
+       document.getElementById('posof').value= posofinput;
+       document.getElementById('preId').value=JSON.parse(qbid);
+       document.getElementById('preK').value= JSON.parse(kqid);
+       closequbitdash();
+       readgenTOD();
+}
+
+function crcpbitlink(label){
+  var list=document.getElementsByClassName("sharebiton");
+  list = [].slice.call(list); 
+  var posofinput = list.indexOf(label);
+  var x = document.getElementsByClassName('qbidst');
+  var y = document.getElementsByClassName('keyidst');
+       var qbid = window.btoa(JSON.parse(x[posofinput].value));
+       var kqid = window.btoa(JSON.parse(y[posofinput].value));
+  var newlk = "https://qubit.mwfbiz.com?qbid="+qbid+"&bizk="+kqid+"&no=yes";
+  copybitlink(newlk);
+}
+
+function copybitlink(newlk){
+  var textA = document.createElement("input");
+  textA.value = newlk;
+  textA.select();
+  textA.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(textA.value);
+  $('#lkcopied').slideDown('fast');
+  setTimeout(function(){$('#lkcopied').slideUp('fast');},2000);
 }
