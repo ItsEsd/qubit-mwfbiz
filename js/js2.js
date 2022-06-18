eca66e8631fc72b3a37cad6f2d1ba37ec4ea0b4e
ItsEsd <81977626+ItsEsd@users.noreply.github.com>
2022-06-18 10:19:46 +0530

    Update js2.js

--- js/js2.js
+++ js/js2.js
@@ -26,13 +26,30 @@ $('#backEditor').click(function(){
     $('#storedbitspre').show();
   });
  
-
+  var user = getCookie("_ybizc0");
+  var usergu = getCookie("_ybizv0");
+  var userel = getCookie("_ybize0");
 var url_string = window.location.href;
 var url = new URL(url_string);
 var q = window.atob(url.searchParams.get("qbid"));
 var k = window.atob(url.searchParams.get("bizk"));
 var r = url.searchParams.get("no");
-if(r =="yes"){
+if(r =="yes" && user!=""){
+  document.getElementById('preId').value= q;
+  document.getElementById('preK').value= k;
+  $('#backEditor,#qbitbody').hide();
+  $('#falqbody').show();
+  readgenTOD();
+}
+else if(r =="yes" && userel!=""){
+  document.getElementById('preId').value= q;
+  document.getElementById('preK').value= k;
+  $('#backEditor,#qbitbody').hide();
+  $('#falqbody').show();
+  readgenTOD();
+}
+else if(r =="yes" && usergu!=""){
+  document.body.style.pointerEvents = 'none';
   document.getElementById('preId').value= q;
   document.getElementById('preK').value= k;
   $('#backEditor,#qbitbody').hide();
@@ -40,6 +57,8 @@ if(r =="yes"){
   readgenTOD();
 }
 else{
+  document.body.style.pointerEvents = 'none';
   $('#qbitbody').show();
+  $('#falqbody').hide();
 }
 
