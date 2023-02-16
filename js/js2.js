2760663e0698a5ddc5729ad5ebd2334b450d0202
S Das <d.soubhik@hotmail.com>
2023-02-17 03:27:06 +0530

    Update js2.js

--- js/js2.js
+++ js/js2.js
@@ -25,7 +25,7 @@ $('#backEditor').click(function(){
     $('#closest').hide();
     $('#storedbitspre').show();
   });
- 
+  function getCookie(cname) { var name = cname + "="; var decodedCookie = decodeURIComponent(document.cookie); var ca = decodedCookie.split(';'); for (var i = 0; i < ca.length; i++) { var c = ca[i]; while (c.charAt(0) == ' ') { c = c.substring(1); } if (c.indexOf(name) == 0) { return c.substring(name.length, c.length); } } return ""; } 
   var user = getCookie("_ybizc0");
   var usergu = getCookie("_ybizv0");
   var userel = getCookie("_ybize0");
